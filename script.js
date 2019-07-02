639c4a10f8f27609ccc501a1b295c4207dc3064e
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-07-02 22:43:04 +0545

    fix: convert to boolean for inStock

--- script.js
+++ script.js
@@ -5,17 +5,61 @@ canvas.height = 600;
 
 const ctx = canvas.getContext('2d');
 
+const randomGenerator = (max, min) => Math.random() * (max - min) + min;
 
-const randomCoordinate = max => Math.floor(Math.random() * max+1);
-
-const spawnBalls = (num) => {
-    for (let i = 0; i<num; i++){
-        ctx.beginPath();
-        ctx.arc(randomCoordinate(1200), randomCoordinate(600), 15, 0, Math.PI * 2, false);
-        ctx.fillStyle = "green";
-        ctx.fill();
-        ctx.closePath();
+class Ant {
+    constructor(x, y, radius, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.dx = dx;
+        this.dy = dy;
     }
 }
 
-spawnBalls(10);
+const ants = [];
+
+
+
+const antGenerator = (x, y, radius) => {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}
+
+const radiusCoordinates = [];
+
+const spawnAnts = (num, radius) => {
+    for (let i = 0; i<num; i++){
+        let x = randomCoordinate(1200 - radius, radius);
+        let y = randomCoordinate(600 - radius, radius);
+        let antObj = {
+            x,
+            y,
+            radius,
+            dx,
+            dy,
+        } 
+        ants.push(antObj);
+    }
+};
+
+let a =50;
+let b= 50;
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    antGenerator(a,b, 12);
+    a += 2;
+    b += -2;
+    console.log(x,y)
+};
+setInterval(draw, 10);
+
+spawnAnts(0, 12);
+
+//radius(x,y) difference
+//obj
+//velocity, (dy,dx) => random
+
